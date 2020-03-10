ce557851c8fe3671e51ade6b1f078022837aad08
evansheehan <awubis@gmail.com>
2020-03-10 16:44:36 -0400

    Initial Commit

---
+++ src/main/com/evansheehan/lesson1/BinaryGap.java
@@ -0,0 +1,5 @@
+package main.com.evansheehan.lesson1;
+
+public class BinaryGap {
+
+}
---
+++ src/test/com/evansheehan/lesson1/BinaryGapTest.java
@@ -0,0 +1,10 @@
+package com.evansheehan.lesson1;
+
+public class BinaryGapTest {
+    public int solution(int N) {
+
+        Integer.toBinaryString(N);
+
+        return 0;
+    }
+}
